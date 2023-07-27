ca94dea37f5bf77d4b156a73086b14ac9afeeeee
Pain <mohammedbrham98@gmail.com>
2023-07-27 11:25:48 +0400

    Update Config_MCLIP.py

--- multilingual_clip/Config_MCLIP.py
+++ multilingual_clip/Config_MCLIP.py
@@ -4,7 +4,7 @@ import transformers
 class MCLIPConfig(transformers.PretrainedConfig):
     model_type = "M-CLIP"
 
-    def __init__(self, modelBase='bert-base-arabertv2', transformerDimSize=1024, imageDimSize=512, **kwargs):
+    def __init__(self, modelBase='aubmindlab/bert-base-arabertv2', transformerDimSize=1024, imageDimSize=512, **kwargs):
         self.transformerDimensions = transformerDimSize
         self.numDims = imageDimSize
         self.modelBase = modelBase
